893070417155966c317895bcfd4d758f2c86fd61
Streamlet <streamlet@outlook.com>
2023-07-13 19:21:48 +0800

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+# 我的 Rime 配置
+---
+
+## 使用方法
+
+### mac
+```bash
+rm -rf ~/Library/Rime
+git clone git@github.com:Streamlet/rime-config.git ~/Library/Rime
+```
+
+---
+
+## 搜狗词库
+```bash
+python tools/install_sogou_dict.py
+```
--- tools/install_sogou_dict.py
+++ tools/install_sogou_dict.py
@@ -6,19 +6,17 @@ import sys
 import shutil
 
 if sys.version >= '3':
-    import urllib.request as urllib
+    import urllib.request as url_request
+    import urllib.parse as url_parse
 else:
-    import urllib
+    import urllib as url_request
+    import urllib as url_parse
 
 
 def download(url, file):
     if not os.path.exists(os.path.dirname(file)):
         os.makedirs(os.path.dirname(file))
-    try:
-        remote = urllib.urlopen(url)
-    except Exception as e:
-        print(e)
-        return False
+    remote = url_request.urlopen(url)
     with open(file, 'wb') as local:
         BLOCK_SIZE = 1024 * 1024
         while True:
@@ -36,20 +34,24 @@ dicts = {
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
-if not os.path.exists('scel2txt'):
-    os.system('git clone https://github.com/lewangdev/scel2txt.git')
+if os.path.exists('scel2txt'):
+    shutil.rmtree('scel2txt')
+os.system('git clone https://github.com/lewangdev/scel2txt.git')
 os.chdir('scel2txt')
 
 for dict in dicts:
     url = 'https://pinyin.sogou.com/d/dict/download_cell.php?id=%d&name=%s' % (
-        dicts[dict], dict)
+        dicts[dict], url_parse.quote(dict))
+    print('下载"%s": %s ...' % (dict, url))
     download(url, os.path.join('scel', dict + '.scel'))
 
-if os.system('which go') == 0:
+if sys.version >= '3':
+    os.system('%s scel2txt.py' % sys.executable)
+elif (sys.platform == 'win32' and os.system('where go') == 0) or (sys.platform != 'win32' and os.system('which go') == 0):
     os.system('go build scel2txt.go')
     os.system('./scel2txt')
 else:
-    os.system('%s scel2txt.py' % sys.executable)
+    assert False, '需要 python3 环境或者 golang 环境'
 
 shutil.copy2(os.path.join('out', 'luna_pinyin.sogou.dict.yaml'),
              os.path.dirname(script_dir))
